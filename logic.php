c5e0480fb24fa68809e5d3f6c170041052d240fa
TheCmar7 <smithcr@flip1.engr.oregonstate.edu>
2015-07-29 14:24:26 -0700

    I added some comments and a connection to thr database

--- logic.php
+++ logic.php
@@ -1,10 +1,14 @@
 <?php
 	include('config.php');
+	$dbc = getdbc();
 
-	$mysql_handle = mysql_connect($dbhost, $dbuser, $dbpass)
-    or die("Error connecting to database server");
+	$query = "INSERT INTO videos(id, name, category, length)";
 
-	mysql_select_db($dbname, $mysql_handle)
-    or die("Error selecting database: $dbname");
+
+	#remove all 
+	#select a genre
+	#rent and return
+	#add a movie
+
 
 ?>
