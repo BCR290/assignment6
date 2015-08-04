f1d5cc49047b992c9c05db1936fc964369965d1a
TheCmar7 <smithcr@flip1.engr.oregonstate.edu>
2015-08-04 14:35:24 -0700

    inothing

--- logic.php
+++ logic.php
@@ -1,14 +1,22 @@
 <?php
-	include('config.php');
+	include("config.php");
 	$dbc = getdbc();
 
-	$query = "INSERT INTO videos(id, name, category, length)";
+	#$query = "INSERT INTO videos(id, name, category, length)";
 
 
 	#remove all 
-	#select a genre
+	if ($_GET["action"] == "removeAll") {
+		echo "remove";
+	}
+	
 	#rent and return
+	#some other $_GET if statements
+
 	#add a movie
+	if ($_GET["action"] == "addmovie") {
+		echo "poop";
+	}
 
 
 ?>
